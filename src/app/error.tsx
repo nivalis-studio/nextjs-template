496e5789b7f33fccda7111b11a97a53969ee5229
pnodet <paul.nodet@gmail.com>
2023-12-15 20:46:31 +0700

    feat: improve 404 and 500 default page layout

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,17 +2,17 @@
 
 import {useEffect} from 'react';
 
-const Error = ({error}: {readonly error: Error}) => {
+const ErrorPage = ({error}: {readonly error: Error}) => {
 	useEffect(() => {
 		// Log the error to an error reporting service
 		console.error(error);
 	}, [error]);
 
 	return (
-		<div>
+		<div className='flex h-full min-h-[50vh] w-full items-center justify-center text-center text-xl font-medium'>
 			<p>Oh no, something went wrong... maybe refresh?</p>
 		</div>
 	);
 };
 
-export default Error;
+export default ErrorPage;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,7 @@
-const Page404 = () => <div>Page Not Found</div>;
+const Page404 = () => (
+	<div className='flex h-full min-h-[50vh] w-full items-center justify-center text-center text-xl font-medium'>
+		Page Not Found
+	</div>
+);
 
 export default Page404;
