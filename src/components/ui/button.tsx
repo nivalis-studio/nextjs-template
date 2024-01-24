6d4a64bb6696d69ed4938ab65998780f2f175dc5
pnodet <paul.nodet@gmail.com>
2024-01-24 14:02:00 +0800

    feat: add shadcn components & format

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,53 +1,54 @@
-import React from 'react';
+import {forwardRef} from 'react';
 import {Slot} from '@radix-ui/react-slot';
 import {cva} from 'class-variance-authority';
 import {cn} from '@/utils/classnames';
+import type {ButtonHTMLAttributes} from 'react';
 import type {VariantProps} from 'class-variance-authority';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-800',
+	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300',
 	{
-		defaultVariants: {
-			size: 'default',
-			variant: 'default',
-		},
 		variants: {
-			size: {
-				'default': 'h-10 px-4 py-2',
-				icon: 'h-10 w-10',
-				lg: 'h-11 rounded-md px-8',
-				sm: 'h-9 rounded-md px-3',
-			},
 			variant: {
-				'default':
+				default:
 					'bg-gray-900 text-gray-50 hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90',
 				destructive:
-					'bg-red-500 text-gray-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-red-50 dark:hover:bg-red-900/90',
-				ghost:
-					'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
-				link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
+					'bg-red-500 text-gray-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90',
 				outline:
 					'border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50',
 				secondary:
 					'bg-gray-100 text-gray-900 hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
+				ghost:
+					'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
+				link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
 			},
+			size: {
+				default: 'h-10 px-4 py-2',
+				sm: 'h-9 rounded-md px-3',
+				lg: 'h-11 rounded-md px-8',
+				icon: 'h-10 w-10',
+			},
+		},
+		defaultVariants: {
+			variant: 'default',
+			size: 'default',
 		},
 	},
 );
 
 export type ButtonProps = {
 	readonly asChild?: boolean;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+} & ButtonHTMLAttributes<HTMLButtonElement> &
 	VariantProps<typeof buttonVariants>;
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 	({className, variant, size, asChild = false, ...props}, ref) => {
 		const Comp = asChild ? Slot : 'button';
 
 		return (
 			<Comp
 				ref={ref}
-				className={cn(buttonVariants({className, size, variant}))}
+				className={cn(buttonVariants({variant, size, className}))}
 				{...props}
 			/>
 		);
--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -3,14 +3,16 @@ import {cn} from '@/utils/classnames';
 const Skeleton = ({
 	className,
 	...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-	<div
-		className={cn(
-			'animate-pulse rounded-md bg-gray-100 dark:bg-gray-800',
-			className,
-		)}
-		{...props}
-	/>
-);
+}: React.HTMLAttributes<HTMLDivElement>) => {
+	return (
+		<div
+			className={cn(
+				'animate-pulse rounded-md bg-gray-100 dark:bg-gray-800',
+				className,
+			)}
+			{...props}
+		/>
+	);
+};
 
 export {Skeleton};
