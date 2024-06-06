abbb9ad96d25d0a89c6090c9e53a6aa439531213
pnodet <paul.nodet@gmail.com>
2024-06-06 15:51:44 +0200

    chore: bump deps

--- src/components/ui/carousel.client.tsx
+++ src/components/ui/carousel.client.tsx
@@ -228,7 +228,7 @@ const CarouselPrevious = forwardRef<
       size={size}
       disabled={!canScrollPrev}
       className={cn(
-        'absolute  size-8 rounded-full',
+        'absolute size-8 rounded-full',
         orientation === 'horizontal'
           ? '-left-12 top-1/2 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
