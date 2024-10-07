13292a6bb72aa3956c0f0ea112726b83a12a04d1
pnodet <paul.nodet@gmail.com>
2024-10-07 23:35:51 +0200

    chore: styles cleanup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='flex size-full flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20'>
+      <p>Hello world!</p>
+    </div>
+  );
+};
+
+export default Home;
