9181741eaf66c631ca49dc61afde14f52e451ef6
pnodet <paul.nodet@gmail.com>
2025-01-22 18:53:26 +0100

    chore: bump deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
 import { nivalis } from '@nivalis/eslint-config';
 
 export default nivalis({
-  typescript: {
-    configPath: './tsconfig.json',
-  },
   tailwindcss: false,
   // tailwindcss: {
   //   configPath: './tailwind.config.ts',
