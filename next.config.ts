bb123c8c4860594854c381dc29052f9c6c10b731
pnodet <paul.nodet@gmail.com>
2025-02-14 01:19:18 +0100

    chore: update experimental

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,48 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: {
     dirs: ['.'],
+    ignoreDuringBuilds: true,
   },
   experimental: {
     ppr: true,
+    serverMinification: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+    // @see https://vercel.com/blog/how-we-optimized-package-imports-in-next-js
+    optimizePackageImports: [
+      '@radix-ui/react-accessible-icon',
+      '@radix-ui/react-dropdown-menu',
+      '@radix-ui/react-label',
+      '@radix-ui/react-scroll-area',
+      '@radix-ui/react-select',
+      '@radix-ui/react-slot',
+      '@radix-ui/react-tabs',
+      '@radix-ui/react-toast',
+      '@radix-ui/react-tooltip',
+      'tailwindcss',
+    ],
   },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      { hostname: 'res.cloudinary.com' },
-      { hostname: 'plus.unsplash.com' },
-      { hostname: 'images.unsplash.com' },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
