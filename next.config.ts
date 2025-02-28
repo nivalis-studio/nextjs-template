d1df60100420d803c6cb91e8e1a979ac617b3936
pnodet <paul.nodet@gmail.com>
2025-02-28 11:13:28 +0100

    chore: bump deps

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    ppr: true,
+    // ppr: true,
     serverMinification: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
