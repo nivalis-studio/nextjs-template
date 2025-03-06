b8cc8969039ed67032e508c8b8ccbcf9740ad384
pnodet <paul.nodet@gmail.com>
2025-03-06 20:03:34 +0100

    chore: bump deps

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className='flex size-full flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20'>
-      <p>Hello world!</p>
+      <p className='text-2xl'>Hello Nivalis!</p>
     </div>
   );
 };
