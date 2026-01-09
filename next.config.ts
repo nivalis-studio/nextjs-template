80361c84e13b18df912c12f551f6b5777a3d5f64
Paul Nodet <5941125+pnodet@users.noreply.github.com>
2026-01-09 17:31:59 +0100

    feat: add coss ui

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
   typedRoutes: true,
-  experimental: {
-    // @see https://vercel.com/blog/how-we-optimized-package-imports-in-next-js
-    optimizePackageImports: [
-      '@radix-ui/react-accessible-icon',
-      '@radix-ui/react-dropdown-menu',
-      '@radix-ui/react-label',
-      '@radix-ui/react-scroll-area',
-      '@radix-ui/react-select',
-      '@radix-ui/react-slot',
-      '@radix-ui/react-tabs',
-      '@radix-ui/react-toast',
-      '@radix-ui/react-tooltip',
-      'tailwindcss',
-    ],
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 const Page = () => {
   return (
-    <main className='flex size-full flex-col items-center justify-center gap-16 p-8 pb-20'>
+    <div className='flex size-full flex-col items-center justify-center gap-16 p-8 pb-20'>
       <p className='text-2xl'>Hello Nivalis!</p>
-    </main>
+    </div>
   );
 };
 
