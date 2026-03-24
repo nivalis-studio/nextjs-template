e3ba7501ccab486e9b0b9c3a93e89635b07a2535
Paul Nodet <5941125+pnodet@users.noreply.github.com>
2026-03-24 12:42:16 +0100

    chore: bump deps and add taze config file

---
+++ taze.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'taze';
+
+export default defineConfig({
+  force: true,
+  write: true,
+  update: false,
+  install: false,
+  recursive: false,
+  interactive: false,
+  includeLocked: true,
+  ignoreOtherWorkspaces: false,
+  mode: 'latest',
+  packageMode: {
+    '@types/node': 'minor',
+  },
+  exclude: [],
+  ignorePaths: [],
+});
