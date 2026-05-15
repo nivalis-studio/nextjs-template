7a66cac5ccab0743b7b5eb27388e41757424d9b7
Paul Nodet <5941125+pnodet@users.noreply.github.com>
2026-05-15 17:27:20 +0200

    chore: cleanup css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
     <main className='flex size-full h-dvh flex-col items-center justify-center gap-16 p-8 pb-20'>
-      <p className='text-2xl'>Hello Nivalis!</p>
+      <p className='fade-in animate-in text-2xl blur-in duration-500'>
+        Hello Nivalis!
+      </p>
     </main>
   );
 };
